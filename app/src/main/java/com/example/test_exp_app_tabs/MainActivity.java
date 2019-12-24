928b703e719e4788c8b93d7095c2443c4c691830
Shingo Konnai <shingok@Shingos-iMac.local>
2019-12-24 14:27:19 -0800

    add some modules to run the app on the emulator of android

---
+++ app/src/main/java/com/example/test_exp_app_tabs/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.test_exp_app_tabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
